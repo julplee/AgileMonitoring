8e85a2cce2cfcf84124ab2264d7d8c2de5625e97
Julien Plée <julplee@gmail.com>
2012-10-09 19:12:24 +0200

    adding first TDD test + code for project

---
+++ AgileMonitoring/AgileMonitoring.DomainModel.Tests/Project/TestProject.cs
@@ -0,0 +1,24 @@
+﻿namespace AgileMonitoring.DomainModel.Tests.Project
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using NUnit.Framework;
+    using AgileMonitoring.DomainModel.Project;
+
+    [TestFixture]
+    public class TestProject
+    {
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Project_SetEndDateAnteriorToBeginDate_ThrowsInvalidOperationException()
+        {
+            var date = DateTime.Now;
+
+            Project project = new Project("Test Project");
+            project.BeginDate = date;
+            project.EndDate = date.AddDays(-1);
+        }
+    }
+}
---
+++ AgileMonitoring/AgileMonitoring.DomainModel/Project/Project.cs
@@ -0,0 +1,47 @@
+﻿namespace AgileMonitoring.DomainModel.Project
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Project
+    {
+        private DateTime _beginDateUtc;
+
+        private DateTime _endDateUtc;
+
+        public Project(string name)
+        {
+            this.Name = name;
+        }
+
+        public DateTime BeginDate
+        {
+            get { return this._beginDateUtc.ToLocalTime(); }
+            set { this._beginDateUtc = value.ToUniversalTime(); }
+        }
+
+        public DateTime EndDate
+        {
+            get
+            {
+                return _endDateUtc.ToLocalTime();
+            }
+            set
+            {
+                var valueUtc = value.ToUniversalTime();
+
+                if (valueUtc < this._beginDateUtc)
+                    throw new InvalidOperationException("The end date cannot be anterior to begin date");
+
+                this._endDateUtc = value;
+            }
+        }
+
+        public string Name { get; set; }
+    }
+}
