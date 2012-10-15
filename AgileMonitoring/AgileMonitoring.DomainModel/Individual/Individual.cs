402cf69d788d30beba6c97a10f679157e4917dd0
OlivierM <martinet.olivier@gmail.com>
2012-10-15 23:56:19 +0200

    Adding Individual Object and his code-first repository

---
+++ AgileMonitoring/AgileMonitoring.DomainModel/Individual/Individual.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AgileMonitoring.DomainModel.Individual
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Individual
+    {
+        /// <summary>
+        /// The individual id
+        /// </summary>
+        public long IndividualId;
+       
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        public String FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
+        public String LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the user.
+        /// </summary>
+        /// <value>
+        /// The name of the user.
+        /// </value>
+        public String UserName { get; set; }
+
+
+
+    }
+}
---
+++ AgileMonitoring/AgileMonitoring.Persistance/Individual/IndividualRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace AgileMonitoring.Persistance.Individual
+{
+    using AgileMonitoring.DomainModel;
+    using System.Data.Entity;
+
+    /// <summary>
+    /// Repository of Individuals
+    /// </summary>
+    class IndividualRepository : DbContext
+    {
+        public DbSet<Individual> Individuals { get; set; }
+    }
+}
