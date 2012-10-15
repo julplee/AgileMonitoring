f2c21740f93f156e5dc949905b5b380fd0e5c7d7
julplemet <julplee@gmail.com>
2012-10-15 22:23:03 +0200

    Updating project class to follow specifications

--- AgileMonitoring/AgileMonitoring.DomainModel/Project/Project.cs
+++ AgileMonitoring/AgileMonitoring.DomainModel/Project/Project.cs
@@ -12,8 +12,6 @@
     {
         private DateTime _beginDateUtc;
 
-        private DateTime _endDateUtc;
-
         public Project(string name)
         {
             this.Name = name;
@@ -29,19 +27,54 @@
         {
             get
             {
-                return _endDateUtc.ToLocalTime();
-            }
-            set
-            {
-                var valueUtc = value.ToUniversalTime();
-
-                if (valueUtc < this._beginDateUtc)
-                    throw new InvalidOperationException("The end date cannot be anterior to begin date");
-
-                this._endDateUtc = value;
+                // TODO calculate this date (consider state)
             }
         }
 
+        public DateTime DueDate { get; set; }
+
         public string Name { get; set; }
+
+        public Individual Owner { get; set; }
+
+        public IList<UserStory> UserStories { get; set; }
+
+        public int TotalDays { get; set; }
+
+        public int RemainingDays
+        {
+            get { return NbWorkingDays(BeginDate, DateTime.Now); }
+        }
+
+        // TODO JPLEE: qu'est ce ça fout la?
+        public static int NbWorkingDays(DateTime startDate, DateTime endDate)
+        {
+            int nbDays = 0;
+
+            if (endDate <= startDate)
+            {
+                return 0;
+            }
+
+            while (endDate > startDate && endDate.DayOfWeek != DayOfWeek.Sunday &&
+                   endDate.DayOfWeek != DayOfWeek.Saturday)
+            {
+                endDate = endDate.AddDays(-1);
+                nbDays++;
+            }
+
+            while (startDate < endDate && startDate.DayOfWeek != DayOfWeek.Saturday &&
+                   startDate.DayOfWeek != DayOfWeek.Sunday)
+            {
+                startDate = startDate.AddDays(1);
+                nbDays++;
+            }
+
+            TimeSpan totalNbDaysSpan = endDate - startDate;
+
+            nbDays += 5*(totalNbDaysSpan.Days/7);
+            return nbDays;
+
+        }
     }
 }
